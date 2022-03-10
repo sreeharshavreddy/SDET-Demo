8cdea4887878f69c50cf166f5432e71f2afdc34c
ADMIN <ADMIN@DESKTOP-7CF6N7D>
2022-03-10 16:32:12 +0530

    my second commit

--- src/main/java/Demo/SDET/sample.java
+++ src/main/java/Demo/SDET/sample.java
@@ -8,6 +8,21 @@ public class sample
 	public void Sample()
 	{
 		System.out.println("User name 1");
+		
+		System.out.println("User name 2");
+		
+		
+	System.out.println("User name 3");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
